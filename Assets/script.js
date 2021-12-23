184e2b8badb45e9092e1828f98418b10b76c4a95
Christian Tanicala <christian.tanicala@gmail.com>
2021-12-23 10:52:45 -0500

    succesful local storage and creation of search history:

--- Assets/script.js
+++ Assets/script.js
@@ -22,11 +22,20 @@ let searchResult = () => {
     fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityEl},${stateEl}US&units=imperial&cnt=7&appid=7aa6a99f3c6b2918ed1aa6023a5c4fdd`)
     .then(response => response.json())
 
-    .then(function (data) {
+    .then(function getData(data) {
         console.log(data);
 
-        displayFuture(data)
+        displayWeather(data)
+        
+        let historyBtnEl = `
+        <div>
+            <button type="button" id="historyBtn" class="button">${cityEl},${stateEl}</button>
+        </div>
+        `
 
+        $(historyBtnEl).appendTo("#history")
+
+        localStorage.setItem("searched" + localStorage.length, [cityEl, stateEl])
         
     })
 
@@ -35,41 +44,14 @@ let searchResult = () => {
     })
 }
 
-
-
-// let displayFuture = (data) => {
-//     let futureForecast = '';
-
-//         for (let i = 1; i < data.length; i++) {
-            
-//             futureForecast += `
-//             <div class="container row col-12" id="forecast">
-//                 <div class="col-2 card forecast-day">
-//                 <h4 id="date-1">Date+1</h4>
-//                 <ol>
-//                     <li id="temp-1">${data.main.temp}</li>
-                    
-//                     <li id="wind-1">Wind+1</li>
-                    
-//                     <li id="humidity-1">Humidity+1</li>
-//                 </ol>
-//             </div>
-//             `
-            
-//         }
-
-//         $(futureForecast).insertAfter("#current-day")
-//         console.log(futureForecast)
-// }
-
-let displayFuture = (data) => {
+let displayWeather = (data) => {
 
     let currentDay = `
         <div class="container col-3" id="current-day">
             
             <h2 id="city-current">${data.city.name} (${data.list[0].dt_txt})</h2>
             <ol>
-            <li id="temp-current">Tempature: ${data.list[0].main.temp} F</li>
+            <li id="temp-current">Tempature: ${data.list[0].main.temp} °F</li>
             
             <li id="wind-current">Wind Speed: ${data.list[0].wind.speed} mph</li>
             
@@ -91,25 +73,26 @@ let displayFuture = (data) => {
                 </ol>
             </div>
             `
+
+
     $("#contentRemoval").empty()
     $(currentDay).appendTo("#contentRemoval")
     $(futureDayOne).insertAfter("#current-day")
 }
 
+let displaySearchHistory = () => {
+    for (let i = 0; i < localStorage.length; i++) {
+        let historyBtnEl = `
+        <div>
+            <button type="button" id="historyBtn" class="button">${cityEl},${stateEl}</button>
+        </div>
+        `
+
+        $(historyBtnEl).appendTo("#history")
+    }
+}
+
+
 // When the search button is clicke it submits the data to the search function to search for city/state
 searchBtn.addEventListener("click", searchResult)
-
-
-
-// fetch(apiLink)
-//     .then(response => response.json())
-//     .then(data => console.log(data));
-    // .then(weather => showWeather(weather.results));
-
-// showWeather() = weather => {
-//     const currentDayDiv = document.querySelector("#current-day");
-
-//     const cityElement = document.createElement('h2');
-//     cityElement.innerText = 
-// }
-
+document.addEventListener("DOMContentLoaded", displaySearchHistory)
